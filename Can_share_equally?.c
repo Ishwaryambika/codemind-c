cdabcf6b9f61ae895dcd93519fbda5e29c424f3a
Ishwaryambika <119868381+Ishwaryambika@users.noreply.github.com>
2023-05-19 19:17:29 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(x!=0)     {         if(x%2==0)         {             printf("YES");         }         else         {             printf("NO");         }     }     else     {         if(y%2==0)         {             printf("YES");         }         else         {             printf("NO");         }     }
    }

---
+++ Can_share_equally?.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(x!=0)
+    {
+        if(x%2==0)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+    else
+    {
+        if(y%2==0)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+    
+}
