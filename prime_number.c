d0908e7a4b9d42ef32cfe2f3f623e64bb9d2d6ea
Ishwaryambika <119868381+Ishwaryambika@users.noreply.github.com>
2023-08-28 22:30:38 +0530

    #include<stdio.h>  int main()  {      int n;      scanf("%d",&n);      if(n%3!=0)      {          printf("prime");      }      else      {          printf("not a prime");      }  }

--- prime_number.c
+++ prime_number.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    if(n%3!=0)
-    {
-        printf("prime");
-    }
-    else
-    {
-        printf("not a prime");
-    }
-}
+ #include<stdio.h>
+ int main()
+ {
+     int n;
+     scanf("%d",&n);
+     if(n%3!=0)
+     {
+         printf("prime");
+     }
+     else
+     {
+         printf("not a prime");
+     }
+ }
